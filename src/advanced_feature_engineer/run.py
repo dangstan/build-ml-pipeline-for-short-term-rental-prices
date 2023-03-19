c320bf07f2c1b0b41094ada6a689ffd5f1445f6c
dangstan <dangstan@github.com>
2023-03-19 13:20:20 -0300

    boruta updated

---
+++ src/advanced_feature_engineer/run.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+import os
+import json
+import feature_engineer as fe
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="advanced_feature_engineer")
+    run.config.update(args)
+
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+    logger.info('data loaded')
+
+    # Drop outliers
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    logger.info('outliers dropped')
+
+    df = fe.interpolating(df)
+    df = fe.get_distance(df)
+    df,groupers = fe.grouping(df)
+    df,dummies = fe.final_adjustments(df)
+
+    boruta_featured = list(fe.reducing_features(df))
+
+    df = df[['last_review','name']+boruta_featured]
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    logger.info('last_review converted to datetime')
+    df.to_csv("featurized.csv", index=False)
+    logger.info('data saved locally')
+
+    if not os.path.exists('groupers'):
+        os.makedirs('groupers')
+    for i in range(len(groupers)):
+        groupers[i].to_csv(f'groupers/grouper_{str(i)}.csv')
+
+    
+    with open('dummies.json', 'w') as f:
+        json.dump(dummies, f)
+    
+    with open('boruta_featured.json', 'w') as f:
+        json.dump(dummies, f)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_dir('groupers')
+    artifact.add_file("dummies.json")
+    artifact.add_file("boruta_featured.json")
+    artifact.add_file("featurized.csv")
+    run.log_artifact(artifact)
+    logger.info('cleaned data uploaded To W&B')
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help='the name for the input artifact',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help='the name for the output artifact',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help='the type for the output artifact',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help='a description for the output artifact',
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help='the minimum price to consider',
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help='the maximum price to consider',
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
---
+++ src/train_lgbm/feature_engineering.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from boruta import BorutaPy
+from lightgbm import LGBMRegressor
+
+def delta_date_feature(dates):
+    """
+    Given a 2d array containing dates (in any format recognized by pd.to_datetime), it returns the delta in days
+    between each date and the most recent date in its column
+    """
+    date_sanitized = pd.DataFrame(dates).apply(pd.to_datetime)
+    return date_sanitized.apply(lambda d: (d.max() -d).dt.days, axis=0).to_numpy()
+
+
+def reducing_features(df):
+
+    run = wandb.init()
+
+    artifact_local_path = run.use_artifact('dangstan/nyc_airbnb/featurized:v1', type='featurized').download()
+
+    boruta_features = pd.read_json(artifact_local_path+'/dummies.json')[0].values.tolist()
+
+    return df[['last_review','name']+boruta_features]
