d828642140fc3c68d57d84c922922a263b407e8d
dangstan <dangstan@github.com>
2023-03-25 13:31:08 -0300

    data_split update

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
                  parameters={
-                    "input": "featurized:latest",
+                    "input": "featurized:v4",
                     "test_size": config['modeling']['test_size'],
                     "random_seed": config['modeling']['random_seed'],
                     "stratify_by": config['modeling']['stratify_by']
