988f6018fb3c8086116385d893ebeb287f9c0685
dangstan <dangstan@github.com>
2023-03-25 13:01:11 -0300

    2

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def go(config: DictConfig):
                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                 "main",
                 parameters={
-                    "ref": "featurized.csv:reference",
+                    "ref": "featurized.csv:latest",
                     "csv": "featurized.csv:latest",
                     "kl_threshold": config['data_check']['kl_threshold'],
                     "min_price": config['etl']['min_price'],
